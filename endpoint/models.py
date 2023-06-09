28372fd7fce16d783f0861dfd9cbcbe727348809
Dmitriy-Bitsenko <bitsenko@inbox.ru>
2023-06-09 17:06:06 +0500

    ...

--- endpoint/models.py
+++ endpoint/models.py
@@ -4,4 +4,4 @@ from django.db import models
 
 
 class File(models.Model):
-    pass
+    name = models.FileField(default='file')
--- endpoint/views.py
+++ endpoint/views.py
@@ -13,3 +13,7 @@ def index(request):
     return render(request, 'index.html')
 
 
+def file():
+    pass
+
+
